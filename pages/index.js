dfdf0e973455527d066d0f801e25f7c5a6c13345
n22012 <n22012@std.it-college.ac.jp>
2023-01-30 00:52:28 +0000

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import Meta from 'components/meta'
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home () {
   return (
-    <Hero
-      title='CUBE'
-      subtitle='アウトプットしていくサイト'
-      imageOn
-    />
+    <Container>
+      <Meta />
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    </Container>
   )
 }
